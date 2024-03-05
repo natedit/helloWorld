797ecff70b8313953c52acc94da97e86da11b273
nateditmars <nditmars@umd.edu>
2024-03-05 18:50:09 -0500

    Final Commit to Push app route for new about-css html page with applied styling

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def hello():  # put application's code here
 def practice():  # put application's code here
     return render_template('about.html')
 
+@app.route('/about-css')
+def practice():  # put application's code here
+    return render_template('about-css.html')
+
 
 if __name__ == '__main__':
     app.run()
